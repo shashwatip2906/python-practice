fccd14b732bda2b673dd7cad23ff0504e721d988
Shashwati Pingalkar <shashwatip2906@gmail.com>
2026-09-18 19:46:00 +0530

    Python Programs on Functions and Modules

---
+++ 05_ Functions and Modules/72_reduced-function.py
@@ -0,0 +1,5 @@
+from functools import reduce
+
+numbers = [1, 2, 3, 4, 5]
+product = reduce(lambda x, y: x * y, numbers)
+print(product)
---
+++ 05_ Functions and Modules/73_args-in-function.py
@@ -0,0 +1,4 @@
+def add(*args):
+    return sum(args)
+
+print(add(10, 20, 30, 40))
